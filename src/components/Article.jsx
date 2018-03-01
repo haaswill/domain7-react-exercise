a76d609eada3c2378418544082196d704933def2
Willian <haaswillian@gmail.com>
2018-02-28 19:34:23 -0800

    Added link to Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardMedia, CardText, CardTitle } from 'material-ui/Card';
 
-const Article = ({ title, image, description, author, datetime }) => (
+const Article = ({ title, image, description, author, datetime, url }) => (
   <Card style={{ margin: 5 }}>
-    <CardMedia overlay={<CardTitle title={title} subtitle={`By ${author} - Published on ${datetime}`} />}>
-      <img src={image} />
-    </CardMedia>
-    <CardText>
-      {description}
-    </CardText>
+    <a href={url} target="_blank" style={{ textDecoration: 'none' }}>
+      <CardMedia overlay={<CardTitle title={title} subtitle={`By ${author} - Published on ${datetime}`} />}>
+        <img src={image} />
+      </CardMedia>
+      <CardText>
+        {description}
+      </CardText>
+    </a>
   </Card>
 );
 
@@ -19,7 +21,8 @@ Article.propTypes = {
   image: PropTypes.string,
   description: PropTypes.string,
   author: PropTypes.string,
-  datetime: PropTypes.string
+  datetime: PropTypes.string,
+  url: PropTypes.string.isRequired
 }
 
 export { Article };
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -18,6 +18,7 @@ const ArticleList = ({ articles, loading }) => {
           image={urlToImage}
           key={url}
           title={title}
+          url={url}
         />
       );
     });
